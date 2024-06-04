ffa48f93d51621087f99a59961b37e5bfe9e7866
Gabriel Campos <160979410+gabrielcdr2@users.noreply.github.com>
2024-06-03 21:51:25 -0300

    Add files via upload

--- program/biblioteca.py
+++ program/biblioteca.py
@@ -5,16 +5,160 @@ users = [
     {"login": "Gabriel", "password": "2004", "acess": "comum"},
     {"login": "albert123", "password": "1968", "acess": "comum"},
     {"login": "bibliotecaria", "password": "123", "acess": "bibliotecária"},
-
 ]
 
+livros_reservados = []
+livros_cadastrados = ["Direito", "Engenharia de Software","Introdução a Computadores","Analise de Requisitos"] 
+
 def main():
-    validation = functions.login(users) # 
+    validation = functions.login(users) 
+
     while True:
         if validation == "administrator":
-            print('Você é um ', validation)
-            break
-        
+            while True:
+                print('')
+                print("-"*25)
+                print(f'{"Biblioteca Virtual:":^25} ')
+                print("-"*25)
+                print("1 - Pesquisar Livros")
+                print("2 - Livros Reservados ")
+                print("3 - Cancelar Reserva")
+                print("4 - Gerenciar Usuários")
+                print("5 - Sair\n")
+
+                selecao = input("Selecione uma das opções acima: ")
+
+                try:
+                    int(selecao)
+                    if int(selecao) > 5:
+                        print('Digite somente as opções válidas.')
+                        continue
+
+                except ValueError:
+                    print('Digite somente as opções validas.')
+                    continue
+                
+                if  selecao == "1":
+                    print("-"*25)
+                    print(f'{"Pesquisar Livros":^25}')
+                    print("-"*25)
+                    
+                    while True:
+                        pesquisa = input("Digite o nome do Livro: ").lower()
+                        disponibilidade = functions.verificar_disponibilidade(pesquisa, livros_cadastrados)
+
+                        if disponibilidade == -1:
+                            print('Livro não encontrado!')
+                            continue
+                        
+                        else:
+                            reservar = input("O livro foi encontrado. Deseja reservar o livro? [S/N] ").lower()
+                            if reservar == "s":
+                                livros_reservados.append(pesquisa)
+                                print("Livro reservado!")
+                                break
+                
+                
+                elif selecao == "2":
+                    print("-"*25)
+                    print(f'{"Livros Reservados:":^25}')
+                    print("-"*25)
+                    for livros in livros_reservados:
+                        print('-',livros)
+                    print('''''')
+
+                elif selecao == "3":
+                    cancelar_reserva = input("Digite o nome do livro que deseja cancelar a reserva: ")
+                    if cancelar_reserva.lower() in livros_reservados:
+                        livros_reservados.remove(cancelar_reserva)
+                        print('Reserva cancelada.')
+
+                elif selecao == "4":
+                    functions.gerenciar_usuarios(users)
+                
+                elif selecao == "5":
+                    break
+
+        elif validation == 'bibliotecária':
+            while True:
+                print("-"*25)
+                print(f'{"Biblioteca Virtual:":^25} ')
+                print("-"*25)
+                print("1 - Pesquisar Livros")
+                print("2 - Gerenciar Livros")
+                print("3 - Sair\n")
+
+                selecao = input("Selecione uma das opções acima: ")
+                
+                if  selecao == "1":
+                    print("-"*25)
+                    print(f'{"Pesquisar Livros":^25}')
+                    print("-"*25)
+
+                    pesquisa = input("Digite o nome do Livro: ")
+
+                    verificar = functions.verificar_disponibilidade(pesquisa, livros_cadastrados)
+
+                    if verificar == -1:
+                        print('Livro não encontrado.')
+
+                    else:
+                        reservar = input("O livro foi encontrado. Deseja reservar o livro? [S/N] ").lower()
+                        if reservar == "s":
+                            livros_reservados.append(pesquisa)
+                            print("Livro reservado!")
+
+                elif selecao == "2":
+                    functions.gerenciar_livros(livros_cadastrados)
+
+                elif selecao == "3":
+                    print("Obrigado por usar a nossa biblioteca!")
+                    break
+
+        else:
+            while True:
+                print("-"*25)
+                print(f'{"Biblioteca Virtual:":^25} ')
+                print("-"*25)
+                print("1 - Pesquisar Livros")
+                print("2 - Livros Reservados")
+                print("3 - Cancelar reserva")
+                print("4 - Sair\n")
+
+                selecao = input("Selecione uma das opções acima: ")
+                
+                if  selecao == "1":
+                    print("-"*25)
+                    print(f'{"Pesquisar Livros":^25}')
+                    print("-"*25)
+
+                    pesquisa = input("Digite o nome do Livro: ")
+
+                    verificar = functions.verificar_disponibilidade(pesquisa, livros_cadastrados)
+
+                    if verificar == -1:
+                        print('Livro não encontrado.')
+
+                    else:
+                        reservar = input("O livro foi encontrado. Deseja reservar o livro? [S/N] ").lower()
+                        if reservar == "s":
+                            livros_reservados.append(pesquisa)
+                            print("Livro reservado!")
+                
+                elif selecao == "2":
+                    print("-"*25)
+                    print(f'{"Livros Reservados:":^25}')
+                    print("-"*25)
+                    for livros in livros_reservados:
+                        print('-',livros)
+                    print('''''')
+                elif selecao == "3":
+                    cancelar_reserva = input("Digite o nome do livro que deseja cancelar a reserva: ")
+                    if cancelar_reserva in livros_reservados:
+                        livros_reservados.remove(cancelar_reserva)
+                elif selecao == "4":
+                    break
+
 if __name__ == "__main__":
     main()
     
--- program/functions.py
+++ program/functions.py
@@ -1,9 +1,5 @@
 import os, time
 
-users = [
-    {"login": "admin", "password": "admin", "acess": "administrator"}
-]
-
 # Login validation function
 def login(dictionary):
     while True: # Create a loop for continue showing the login screen until the user enter valid informations 
@@ -31,3 +27,180 @@ def login(dictionary):
                 time.sleep(2)
                 os.system("cls") # cleans the terminal
                 break
+
+# Funções do Administrador
+
+# Pesquisar Livros
+def verificar_disponibilidade(pesquisa, livros_cadastrados):
+    livros_cadastrados.sort()
+    inicio, fim = 0, len(livros_cadastrados) - 1
+
+    while inicio <= fim:
+        meio = (inicio+fim)//2
+
+        if livros_cadastrados[meio].lower() == pesquisa:
+            return meio
+        elif livros_cadastrados[meio].lower() < pesquisa:
+            inicio = meio + 1
+        else:
+            fim = meio - 1            
+    
+    return -1
+
+# Gerenciador de usuários
+def gerenciar_usuarios(users):
+    while True:
+        print("\n1 - Cadastrar Usuários")
+        print("2 - Deletar Usuários")
+        print("3 - Listar Usuários")
+        print("4 - Voltar ao início")
+
+        selecao = input("Selecione uma das opções acima: ")
+
+        try:
+            int(selecao)
+            if int(selecao) > 4:
+                print('Digite somente as opções válidas.')
+                continue
+
+        except ValueError:
+            print('Digite somente as opções validas.')
+            continue
+        
+        if selecao == "1":
+            cadastrar_usuario(users)
+
+        elif selecao == "2":
+            deletar = input("\nDigite o usuário que deseja deletar: ")
+            deletou = False
+
+            for usuario in users:
+                if deletar == usuario["login"]:
+                    users.remove(usuario)
+                    print("Usuário deletado.")
+                    deletou = True
+                    break
+
+            if not deletou:
+                print('Usuario não encontrado.')
+                break
+
+        elif selecao == "3":
+            print("\nLista de Usuários:")
+            for usuario in users:
+                print(usuario["login"], usuario["acess"])
+        elif selecao == "4":
+            break
+
+# Cadastrar os usuários
+def cadastrar_usuario(users):
+    while True:
+        print("-"*25)
+        print(f'{"Cadastro de usuários":^25}')
+        print("-"*25)
+
+        login = input("login: ")
+        senha = input("Senha: ")
+        acesso = input("Acesso: ")
+
+        for user in users:
+            if login == user["login"]:
+                print('Usuario já existente.')
+                continue
+
+        users.append({"login":login, "password":senha, "acess":acesso})
+        print('Usuário cadastrado!')
+        criar_log(users, login)
+        break
+
+# Gerenciar livros
+def gerenciar_livros(livros_cadastrados):
+    while True:
+        print("-"*27)
+        print(f'{"Gerenciamento de Livros":^27}')
+        print("-"*27)
+        print("1 - Adicionar Livro ")
+        print("2 - Remover Livros ")
+        print("3 - Listar Livros ")
+        print("4 - Voltar a Pagina Inicial")
+        Escolha = input("\nEscolha uma das alternativas: ")
+     
+        if Escolha == "1": 
+            print("-"*25)
+            print("Tela de Adicionar Livro")
+            print("-"*25)
+            titulo = input("Digite o Titulo: ")
+            print("-"*25)
+            autor = input("Digite o Nome Autor: ")
+            print("-"*25)
+            data_de_lançamento = input("Digite a Data de Lançamento: ")
+            print("-"*25)
+            print("Livro Cadastrado!!")
+            print("-"*25)
+            livros_cadastrados.append(titulo)
+        
+        elif Escolha == "2": 
+            Nome_do_livro_remover = input("Digite o nome do Livro que vc quer Remover: ")
+            if Nome_do_livro_remover in livros_cadastrados:
+                livros_cadastrados.remove(Nome_do_livro_remover)
+            
+        
+        elif Escolha == "3":
+            print("-"*25)
+            print(f'{"Lista de livros:":^25}')
+            print("-"*25)
+            for livros in livros_cadastrados:
+                print('-',livros)
+        elif Escolha == "4": 
+            break
+        
+        else: 
+            print("escolha uma opção válida")
+
+        # Gerenciar livros
+def gerenciar_livros(livros_cadastrados):
+    while True:
+        print("-"*27)
+        print(f'{"Gerenciamento de Livros":^27}')
+        print("-"*27)
+        print("1 - Adicionar Livro ")
+        print("2 - Remover Livros ")
+        print("3 - Listar Livros ")
+        print("4 - Voltar a Pagina Inicial")
+        Escolha = input("\nEscolha uma das alternativas: ")
+     
+        if Escolha == "1": 
+            print("-"*25)
+            print("Tela de Adicionar Livro")
+            print("-"*25)
+            titulo = input("Digite o Titulo: ")
+            print("-"*25)
+            autor = input("Digite o Nome Autor: ")
+            print("-"*25)
+            data_de_lançamento = input("Digite a Data de Lançamento: ")
+            print("-"*25)
+            print("Livro Cadastrado!!")
+            print("-"*25)
+            livros_cadastrados.append(titulo)
+        
+        elif Escolha == "2": 
+            Nome_do_livro_remover = input("Digite o nome do Livro que vc quer Remover: ")
+            if Nome_do_livro_remover in livros_cadastrados:
+                livros_cadastrados.remove(Nome_do_livro_remover)
+            
+        
+        elif Escolha == "3":
+            print("-"*25)
+            print(f'{"Lista de livros:":^25}')
+            print("-"*25)
+            for livros in livros_cadastrados:
+                print('-',livros)
+        elif Escolha == "4": 
+            break
+        
+        else: 
+            print("escolha uma opção válida")
+
+def criar_log(users, nome):
+    with open('log', 'w') as log:
+        log.write(f"O usuario {nome} foi adicionado ao sistema!")
